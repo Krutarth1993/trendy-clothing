b8a916be9d990d3b2aaa3b19cf4c8b3354d48624
Krutarth1993 <kpj3111993@gmail.com>
2022-08-26 14:36:34 -0400

    Adding Routes to Home page
    Used the Routes, Route, BrowserRoute, Outlet components from react-router-dom.
    
    And used Navigation component from React library.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import CategoryMenu from "./components/category-menu/CategoryMenu";
+import Home from "./routes/home/Home";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/Navigation";
+
+const Shop = () => {
+  return <h1>Shop Page</h1>;
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <CategoryMenu categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
